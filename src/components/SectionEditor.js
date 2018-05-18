cca526463394f02529ce2537063a9c39dfc60484
Antonio de Padua <padua.fregoso@smartplace.mx>
2018-05-18 02:36:06 -0500

    added multi selection

--- src/components/SectionEditor.js
+++ src/components/SectionEditor.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import Menu from "./Menu";
 import {seatType} from "../utils/types";
 import {green} from "../utils/colors";
+import Selector from "./Selector";
 
 
 const getCodeLabel = (offset, startLabel) => {
@@ -16,12 +17,6 @@ const getCodeLabel = (offset, startLabel) => {
 
 const LETTERS_SPACE = 50;
 const CIRCLE_WIDTH = .7;
-
-const onSeatClick = (index, selected) => ({selectedIndexes}) => ({
-    selectedIndexes: selected
-        ? selectedIndexes.filter(i => i !== index)
-        : selectedIndexes.concat(index)
-});
 
 const history = [];
 
@@ -100,7 +95,15 @@ export default class SectionEditor extends React.PureComponent {
         }
     });
 
-    onCircleContextMenu = (e, index, selected) => {
+    onSeatClick = (e, index, selected) => {
+        this.setState(({selectedIndexes}) => ({
+            selectedIndexes: selected
+                ? selectedIndexes.filter(i => i !== index)
+                : selectedIndexes.concat(index)
+        }))
+    };
+
+    onSeatContextMenu = (e, index, selected) => {
         e.preventDefault();
         const {clientX, clientY} = e;
         this.setState(() => ({
@@ -146,6 +149,33 @@ export default class SectionEditor extends React.PureComponent {
         selectedIndexes: []
     }));
 
+    onMultiSelect = ({x1, x2, y1, y2}) => {
+        const point = this.svg.createSVGPoint();
+        point.x = x1;
+        point.y = y1;
+        const {x: X1, y: Y1} = point.matrixTransform(this.svg.getScreenCTM().inverse());
+        point.x = x2;
+        point.y = y2;
+        const {x: X2, y: Y2} = point.matrixTransform(this.svg.getScreenCTM().inverse());
+
+        this.setState(({selectedIndexes, circles}) => {
+            const indexesInRange = circles.map(({x, y, radium}, i) => (
+                    x - radium >= X1
+                    && x + radium <= X2
+                    && y - radium >= Y1
+                    && y + radium <= Y2
+                ) ? i : NaN
+            ).filter(n => !isNaN(n));
+            return {
+                selectedIndexes: indexesInRange.length === 0
+                    ? []
+                    : selectedIndexes.concat(indexesInRange.filter(
+                        i => !selectedIndexes.includes(i)
+                    ))
+            };
+        })
+    };
+
 
     render() {
         const {
@@ -177,31 +207,33 @@ export default class SectionEditor extends React.PureComponent {
                 <input value={rowStartLabel} placeholder="Inicio filas" onChange={e => this.setState({rowStartLabel: e.target.value})}/>
                 <input value={colStartLabel} placeholder="Inicio columnas" onChange={e => this.setState({colStartLabel: e.target.value})}/>
                 <button onClick={this.generate}>Generar</button>
-                <svg style={styles.svg} viewBox={viewBox}>
-                    {rowLabels.map(({row, y}) => (
-                        <text
-                            key={row}
-                            x={20}
-                            y={y}
-                            style={{fontSize: '1.2rem'}}
-                            fontFamily="Proxima Nova"
-                        >
-                            {row}
-                        </text>
-                    ))}
-                    {circles.map((circle, i) => {
-                        const selected = selectedIndexes.includes(i);
-                        return (
-                            <Seat
-                                key={i}
-                                seat={circle}
-                                onContextMenu={e => this.onCircleContextMenu(e, i, selected)}
-                                onClick={() => this.setState(onSeatClick(i, selected))}
-                                selected={selected}
-                            />
-                        )
-                    })}
-                </svg>
+                <Selector onSelect={this.onMultiSelect}>
+                    <svg style={styles.svg} viewBox={viewBox} ref={svg => this.svg = svg}>
+                        {rowLabels.map(({row, y}) => (
+                            <text
+                                key={row}
+                                x={20}
+                                y={y}
+                                style={{fontSize: '1.2rem'}}
+                                fontFamily="Proxima Nova"
+                            >
+                                {row}
+                            </text>
+                        ))}
+                        {circles.map((circle, i) => {
+                            const selected = selectedIndexes.includes(i);
+                            return (
+                                <Seat
+                                    key={i}
+                                    seat={circle}
+                                    onContextMenu={e => this.onSeatContextMenu(e, i, selected)}
+                                    onClick={e => this.onSeatClick(e, i, selected)}
+                                    selected={selected}
+                                />
+                            )
+                        })}
+                    </svg>
+                </Selector>
                 <Menu
                     open={menuOpen}
                     onClose={() => this.setState({menuOpen: false})}
@@ -252,6 +284,10 @@ const Seat = ({seat, selected, ...props}) => {
                 cy={y}
                 r={radium}
                 style={styles.circle}
+                onMouseDown={e => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                }}
             />
             <text
                 x={x}
---
+++ src/components/Selector.js
@@ -0,0 +1,100 @@
+import * as React from 'react'
+import PropTypes from 'prop-types'
+import * as ReactDOM from "react-dom";
+
+const initialState = {
+    startX: 0,
+    endX: 0,
+    startY: 0,
+    endY: 0,
+    show: false
+};
+
+export default class Selector extends React.PureComponent {
+    static propTypes = {
+        children: PropTypes.element.isRequired,
+        onSelect: PropTypes.func.isRequired
+    };
+
+    state = initialState;
+    container = document.createElement('div');
+
+    componentDidMount() {
+        document.body.appendChild(this.container);
+    }
+
+    componentWillUnmount() {
+        document.body.removeChild(this.container);
+    }
+
+    onMouseDown = e => {
+        e.preventDefault();
+        const {clientX, clientY} = e;
+        this.setState({
+            startX: clientX,
+            endX: clientX,
+            startY: clientY,
+            endY: clientY,
+            show: true
+        });
+    };
+
+    onMouseMove = ({clientX, clientY}) => this.setState({
+        endX: clientX,
+        endY: clientY
+    });
+
+    onMouseUp = () => this.setState(({startX, endX, startY, endY}, {onSelect}) => {
+        onSelect({
+            x1: Math.min(startX, endX),
+            x2: Math.max(startX, endX),
+            y1: Math.min(startY, endY),
+            y2: Math.max(startY, endY),
+        });
+        return initialState;
+    });
+    onMouseLeave = () => this.setState(initialState);
+
+    render() {
+        const {children} = this.props;
+        const {
+            show,
+            startX,
+            startY,
+            endX,
+            endY
+        } = this.state;
+
+        const styles = {
+            element: {
+                ...children.props.style,
+                cursor: 'crosshair'
+            },
+            selector: {
+                border: '1px solid #444',
+                background: 'rgba(0,0,0,.5)',
+                position: 'fixed',
+                top: Math.min(startY, endY),
+                left: Math.min(startX, endX),
+                height: Math.abs(startY - endY),
+                width: Math.abs(startX - endX),
+                zIndex: 10,
+                pointerEvents: 'none'
+            },
+        };
+        return (
+            <React.Fragment>
+                {React.cloneElement(children, {
+                    style: styles.element,
+                    onMouseDown: this.onMouseDown,
+                    onMouseMove: show ? this.onMouseMove : undefined,
+                    onMouseUp: show ? this.onMouseUp : undefined,
+                    onMouseLeave: show ? this.onMouseLeave : undefined,
+                })}
+                {ReactDOM.createPortal(show && (
+                    <div style={styles.selector}/>
+                ), this.container)}
+            </React.Fragment>
+        )
+    }
+}
